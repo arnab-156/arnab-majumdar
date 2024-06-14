8528ea4a609d25a99e4f3b27be1a09f828e70ebc
arnab-156 <vendmyt@gmail.com>
2024-06-14 11:22:09 -0400

    css changes, semantic tags div -> section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,14 @@ export default function RootLayout({
                 <nav className="flex flex-col items-center justify-between">
                     <div className="z-10 w-full items-center justify-between font-mono text-sm">
                         <section className="group fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 hover:h-64 hover:bg-yellow-600 hover:opacity-90">
-                            <span className="group-hover:hidden">Welcome!</span>
+                            <span className="animate-bounce group-hover:hidden">Welcome!</span>
                             <div className="hidden group-hover:block">
                                 <Link className="p-2 m-2 hover:text-lg" href="/tech">
                                     technology
                                 </Link>
                                 <Link className="p-2 m-2 hover:text-lg" href="/lotus">lotus mahal</Link>
-                                <Link className="p-2 m-2 hover:text-lg" href="/dashboard">login</Link>
+                                {/* <Link className="p-2 m-2 hover:text-lg" href="/dashboard">login</Link> */}
+                                <Link className="p-2 m-2 hover:text-lg" href="/">home</Link>
                             </div>
                             <Image
                                 className="hidden group-hover:block relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert rounded-2xl mr-2"
@@ -35,7 +36,7 @@ export default function RootLayout({
                                 priority
                             />
                         </section>
-                        <div className="group fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black hover:bg-yellow-600 hover:h-22">
+                        <section className="group fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black hover:bg-yellow-600 hover:h-22">
                             <span className="animate-pulse group-hover:hidden">Copy rights information. All Rights Reserved.</span>
                             <div className="hidden group-hover:block m-4 mt-0">
                                 <p className="mb-2">
@@ -70,7 +71,7 @@ export default function RootLayout({
                                 </div>
                             </div>
 
-                        </div>
+                        </section>
                     </div>
                 </nav>
                 <main>{children}</main>
