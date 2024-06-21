c801aa5c779dbc1eada110590b30b998d1b3fc83
arnab-156 <vendmyt@gmail.com>
2024-06-21 08:56:14 -0400

    some more clean ups

--- src/app/components/__tests__/navigation.test.tsx
+++ src/app/components/__tests__/navigation.test.tsx
@@ -1,15 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import { Navigation } from '../navigation'
+import { Navigation } from '../navigation';
 
 test('loads and displays greeting', async () => {
-    render(<Navigation />)
+    render(<Navigation />);
     // ACT
     //   await userEvent.click(screen.getByText('Lotus'))
     //   await screen.findByRole('heading')
 
-    expect(screen.getByText('technology')).toBeInTheDocument()
-    expect(screen.getByText('home')).toBeInTheDocument()
-    expect(screen.getByText('lotus mahal')).toBeInTheDocument()
-})
+    expect(screen.getByText('technology')).toBeInTheDocument();
+    expect(screen.getByText('home')).toBeInTheDocument();
+    expect(screen.getByText('lotus mahal')).toBeInTheDocument();
+});
