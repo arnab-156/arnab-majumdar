4cd9e8afadeefdeb33a903089ce8d9cb3d9e3bb7
arnab-156 <vendmyt@gmail.com>
2024-06-21 11:36:04 -0400

    Card improvements (#8)
    * card improvements
    
    * test added for card and some cards improved in moc

--- src/app/moc/page.tsx
+++ src/app/moc/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Card } from "../components/card"
+import { ApparelIcon } from "../components/icons";
 
 const urlImageDefault = "/headshot.gif"
 const defaultUrl = ""
@@ -10,35 +11,37 @@ export default function MoC() {
       <div className="mb-32 grid text-center lg:w-half lg:max-w-5xl lg:grid-cols-3 lg:text-left">
         <h1 className="text-3xl font-bold text-center mt-8 mb-16">Made of Chicago</h1>
         <p className="text-xl font-bold text-center mt-8">
-          Concept
+          Made of ChicagoⓇ is a Chicago based fashion active wear brand primarily selling on custom designed compact vending machines!
         </p>
         <Card
           title="More information to come!"
           url="#"
-          imageUrl={urlImageDefault}
           description="more information to come."
-        />
+        >
+          <ApparelIcon height="100px" width="100px" />
+        </Card>
 
         <Card
           title="More information to come!"
           url="#"
-          imageUrl={urlImageDefault}
           description="more information to come."
-        />
-
+        >
+          <ApparelIcon height="100px" width="100px" />
+        </Card>
         <Card
           title="More information to come!"
           url="#"
-          imageUrl={urlImageDefault}
           description="more information to come."
-        />
-
+        >
+          <ApparelIcon height="100px" width="100px" />
+        </Card>
         <Card
           title="More information to come!"
           url="#"
-          imageUrl={urlImageDefault}
           description="more information to come."
-        />
+        >
+          <ApparelIcon height="100px" width="100px" />
+        </Card>
 
       </div>
     </main>
