03bda7e092eacfb860533a1f0a7bf128e372a81c
arnab-156 <vendmyt@gmail.com>
2024-06-22 09:56:36 -0400

    lotus mahal page animation (#13)

--- src/app/lotus/layout.tsx
+++ src/app/lotus/layout.tsx
@@ -1,11 +1,14 @@
+import classNames from "classnames";
+import styles from "./styles.module.css";
+
 export default function LotusLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
-        <section className="bg-gradient-to-t from-pink-200 to-white">
-            <div className="h-2 bg-gradient-to-r from-pink-200 via-white to-blue-200" />
+        <section className={classNames("bg-gradient-to-t from-pink-200 to-white", styles.gradientAnimation)}>
+            <div className={classNames(styles.gradient, styles.gradientAnimation)} />
             {children}
         </section>
     )
