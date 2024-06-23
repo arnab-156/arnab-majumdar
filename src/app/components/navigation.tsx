b9331e35ae5e1fed3c0bf146d0e821845aafaa3c
arnab-156 <vendmyt@gmail.com>
2024-06-23 09:14:14 -0400

    navigation improvements (#19)

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image";
 import Link from 'next/link';
-import { MenuIcon } from "../components/icons";
+import { MenuIcon, LotusIcon } from "../components/icons";
 
 export const Navigation = () => {
     return (<nav className="flex flex-col items-center justify-between">
         <div className="z-10 w-full items-center justify-between flex justify-content font-mono text-sm">
-            <section className="group rounded fixed flex w-3/4 ml-[13%] hover:w-full hover:ml-0 sm:ml-0 sm:w-full lg:w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 py-3 backdrop-blur-md dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 hover:h-fit hover:min-h-60 hover:bg-yellow-600 dark:hover:bg-black hover:opacity-90 left-0 bottom-3 lg:top-0">
+            <section className="group rounded-[36px] fixed flex w-3/4 ml-[13%] hover:w-[96%] hover:sm:w-full hover:ml-2 hover:sm:ml-0 sm:ml-0 sm:w-full lg:w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 py-3 backdrop-blur-md dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 hover:h-fit hover:min-h-60 hover:bg-yellow-600 dark:hover:bg-black hover:opacity-90 left-0 bottom-3 lg:top-0">
                 <span className="group-hover:hidden">
                     <button type="button" className="focus:outline-none">
                         <MenuIcon height="1.5rem" width="1.5rem" />
                     </button>
                 </span>
                 <div className="hidden grid-cols-3 gap-8 group-hover:grid sm:grid-cols-4 sm:gap-2">
-                    <Link className="p-2 m-2 hover:text-lg hover:underline" href="/tech">
+                    <Link className="flex justify-center items-center hover:underline" href="/lotus">
+                        <span className="p-2 m-2 block text-blue-500 sm:hidden"><LotusIcon height={"50px"} width={"50px"} className="hover:bg-pink-300 hover:rounded-full" /></span>
+                        <span className="hidden sm:block hover:text-lg">lotus mahal</span>
+                    </Link>
+
+                    <Link className="flex justify-center items-center p-2 m-2 hover:text-lg hover:underline" href="/tech">
                         technology
                     </Link>
-                    <Link className="p-2 m-2 hover:text-lg hover:underline" href="/lotus">lotus mahal</Link>
+
                     {/* <Link className="p-2 m-2 hover:text-lg hover:underline" href="/dashboard">login</Link> */}
-                    <Link className="p-2 m-2 hover:text-lg hover:underline" href="/">home</Link>
-                    <Link className="" href="/about">
+                    <Link className="flex justify-center items-center p-2 m-2 hover:text-lg hover:underline" href="/">home</Link>
+                    <Link className="flex justify-center items-center" href="/about">
                         <Image
                             className="hidden sm:group-hover:block relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] rounded-2xl mr-2"
                             src="/headshot.gif"
@@ -31,10 +36,8 @@ export const Navigation = () => {
                         <span className="p-2 m-2 block sm:hidden hover:text-lg hover:underline">about</span>
                     </Link>
                 </div>
-
-
             </section>
-            <section className="group fixed left-0 top-0 lg:top-auto lg:bottom-0 flex w-full items-end justify-center bg-gradient-to-b from-zinc-200 via-white dark:from-black dark:via-black hover:bg-yellow-600 hover:h-fit">
+            <section className="group fixed left-0 top-0 lg:top-auto lg:bottom-0 flex w-full items-end justify-center bg-gradient-to-b from-zinc-200 via-white dark:from-black dark:via-black hover:bg-yellow-600 hover:h-fit rounded-[36px]">
                 <span className="animate-pulse mt-2 group-hover:hidden">Copy rights information. All Rights Reserved.</span>
                 <div className="hidden group-hover:block m-4 mt-0">
                     <p className="mb-2 mt-2">
