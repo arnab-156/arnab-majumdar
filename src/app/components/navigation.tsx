2dd99c6f28d10ad819cd5f800c8bc7d0d18fe8b2
arnab-156 <vendmyt@gmail.com>
2024-06-23 09:24:35 -0400

    small screen gap changed

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -5,13 +5,13 @@ import { MenuIcon, LotusIcon } from "../components/icons";
 export const Navigation = () => {
     return (<nav className="flex flex-col items-center justify-between">
         <div className="z-10 w-full items-center justify-between flex justify-content font-mono text-sm">
-            <section className="group rounded-[36px] fixed flex w-3/4 ml-[13%] hover:w-[96%] hover:sm:w-full hover:ml-2 hover:sm:ml-0 sm:ml-0 sm:w-full lg:w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 py-3 backdrop-blur-md dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 hover:h-fit hover:min-h-60 hover:bg-yellow-600 dark:hover:bg-black hover:opacity-90 left-0 bottom-3 lg:top-0">
+            <section className="group rounded-[36px] fixed flex w-3/4 ml-[13%] hover:w-[96%] hover:sm:w-full hover:ml-2 hover:sm:ml-0 sm:ml-0 sm:w-full lg:w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 py-3 backdrop-blur-md dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 hover:h-fit hover:bg-yellow-600 dark:hover:bg-black hover:opacity-90 left-0 bottom-3 lg:top-0">
                 <span className="group-hover:hidden">
                     <button type="button" className="focus:outline-none">
                         <MenuIcon height="1.5rem" width="1.5rem" />
                     </button>
                 </span>
-                <div className="hidden grid-cols-3 gap-8 group-hover:grid sm:grid-cols-4 sm:gap-2">
+                <div className="hidden grid-cols-3 group-hover:grid sm:grid-cols-4 sm:gap-2">
                     <Link className="flex justify-center items-center hover:underline" href="/lotus">
                         <span className="p-2 m-2 block text-blue-500 sm:hidden"><LotusIcon height={"50px"} width={"50px"} className="hover:bg-pink-300 hover:rounded-full" /></span>
                         <span className="hidden sm:block hover:text-lg">lotus mahal</span>
