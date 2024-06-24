7323c366a40bf7cdf0e8e3691d6c11939cc6456d
arnab-156 <vendmyt@gmail.com>
2024-06-24 11:39:09 -0400

    navigation cleaning up

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -5,7 +5,13 @@ import { MenuIcon, LotusIcon } from "../components/icons";
 export const Navigation = () => {
     return (<nav className="flex flex-col items-center justify-between">
         <div className="z-10 w-full items-center justify-between flex justify-content font-mono text-sm">
-            <section className="group rounded-[36px] fixed flex w-3/4 ml-[13%] hover:w-[96%] hover:sm:w-full hover:ml-2 hover:sm:ml-0 sm:ml-0 sm:w-full lg:w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 py-3 backdrop-blur-md dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 hover:h-fit hover:bg-yellow-600 dark:hover:bg-black hover:opacity-90 left-0 bottom-3 lg:top-0">
+            <section className={`group rounded-[36px] fixed flex w-3/4 ml-[13%] sm:ml-0 py-3 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-md 
+             dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit 
+             sm:w-full lg:w-full lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 
+             hover:w-[96%] hover:sm:w-full hover:ml-2 hover:sm:ml-0 hover:h-fit hover:bg-yellow-600 dark:hover:bg-black hover:opacity-90 
+             left-0 bottom-3 lg:top-0 
+             transition ease-in-out group-hover:duration-300
+             `}>
                 <span className="group-hover:hidden">
                     <button type="button" className="focus:outline-none" aria-label="open menu to navigate">
                         <MenuIcon height="1.5rem" width="1.5rem" />
@@ -20,8 +26,6 @@ export const Navigation = () => {
                     <Link className="flex justify-center items-center p-2 m-2 hover:text-lg hover:underline" href="/tech">
                         technology
                     </Link>
-
-                    {/* <Link className="p-2 m-2 hover:text-lg hover:underline" href="/dashboard">login</Link> */}
                     <Link className="flex justify-center items-center p-2 m-2 hover:text-lg hover:underline" href="/">home</Link>
                     <Link className="flex justify-center items-center" href="/about">
                         <Image
@@ -38,7 +42,7 @@ export const Navigation = () => {
                 </div>
             </section>
             <section className="group fixed left-0 top-0 lg:top-auto lg:bottom-0 flex w-full items-end justify-center bg-gradient-to-b from-zinc-200 via-white dark:from-black dark:via-black hover:bg-yellow-600 hover:h-fit rounded-[36px]">
-                <span className="animate-pulse mt-2 group-hover:hidden">Copy rights information. All Rights Reserved.</span>
+                <span className="animate-pulse mt-2 group-hover:hidden">Copy rights and contact information. All Rights Reserved.</span>
                 <div className="hidden group-hover:block m-4 mt-0">
                     <p className="mb-2 mt-2">
                         My Gallup CliftonStrengths are <strong>Input, Achiever, Ideation, Futuristic, Learner</strong>.
