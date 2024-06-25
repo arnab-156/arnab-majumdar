5375d1184deed2c3347d3567c47412dd228b35c0
arnab-156 <vendmyt@gmail.com>
2024-06-25 10:29:54 -0400

    spell

--- src/app/tech/comic/page.tsx
+++ src/app/tech/comic/page.tsx
@@ -5,7 +5,7 @@ import { Comic } from "@/app/components/comic";
 export default function ComicPage() {
   return (
     <main className="flex min-h-screen flex-col pt-8 rounded content-center justify-center m-0">
-      <h1 className="text-xl font-bold text-center mt-8">Enjoy today{"'"}s Comic!</h1>
+      <h1 className="text-xl font-bold text-center mt-8">Enjoy Today{"'"}s Comic!</h1>
       <ComicProvider>
         <Comic />
       </ComicProvider>
