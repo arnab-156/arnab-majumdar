4e4c21fb1d2ca8184528164ed3967ba40cd48180
arnab-156 <vendmyt@gmail.com>
2024-06-25 21:38:04 -0400

    made of chicago page:

--- src/app/components/tile.tsx
+++ src/app/components/tile.tsx
@@ -9,18 +9,19 @@ interface MyTileComponent {
     shadowColor?: string;
     customClassName?: string;
     imageContain?: boolean;
+    imagePosition?: string;
 }
 
-export const Tile: NextPage<MyTileComponent> = ({ title, subTitle, href, url, shadowColor, customClassName, imageContain }) => {
+export const Tile: NextPage<MyTileComponent> = ({ title, subTitle, href, url, shadowColor, customClassName, imageContain, imagePosition = "" }) => {
     return (
         <Link
             href={href}
-            className={`m-4 shadow-md h-96 rounded relative bg-white ${imageContain ? "bg-contain bg-no-repeat" : "bg-cover"} 
+            className={`m-4 shadow-md h-96 rounded relative bg-white ${imageContain ? `bg-contain bg-no-repeat ${imagePosition}` : "bg-cover"} 
           ${url} ${shadowColor ? shadowColor : "shadow-pink-800"} dark:shadow-gray-800
           hover:drop-shadow-lg hover:scale-105 hover:duration-300  ${customClassName}
           `}
         >
-            <div className="p-4 m-2 bg-white/40 rounded absolute bottom-0 dark:text-black">
+            <div className="p-4 m-2 bg-white/80 rounded absolute bottom-0 dark:text-black">
                 <h2 className="font-bold text-overflow-ellipsis overflow-hidden line-clamp-1">{title}</h2>
                 <h3 className="text-overflow-ellipsis overflow-hidden line-clamp-3 text-xs">
                     {subTitle}
