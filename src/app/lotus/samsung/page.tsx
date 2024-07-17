ad60b6d9fe3eae2b9319488b9caf81fbfef437d6
arnab-156 <vendmyt@gmail.com>
2024-07-17 12:58:14 -0400

    samsung att padding

--- src/app/lotus/samsung/page.tsx
+++ src/app/lotus/samsung/page.tsx
@@ -28,19 +28,19 @@ export default function Samsung() {
       />
 
       <div className="flex overflow-x-auto scroll-smooth m-auto mt-6">
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851018356_599f1710a9_w.jpg" alt="att samsung garmemt example 1" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851021701_6619ae0f97_w.jpg" alt="att samsung garmemt example 2" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851281323_30b755d613_w.jpg" alt="att samsung garmemt example 3" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851018531_948efb65d4_w.jpg" alt="att samsung garmemt example 4" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53850713312_f63dfef21a_w.jpg" alt="att samsung garmemt example 4.5" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851018356_599f1710a9_w.jpg" alt="att samsung garmemt example 1" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851021701_6619ae0f97_w.jpg" alt="att samsung garmemt example 2" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851281323_30b755d613_w.jpg" alt="att samsung garmemt example 3" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851018531_948efb65d4_w.jpg" alt="att samsung garmemt example 4" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53850713312_f63dfef21a_w.jpg" alt="att samsung garmemt example 4.5" />
       </div>
 
       <div className="flex overflow-x-auto scroll-smooth m-auto my-6">
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851453880_923c1cbe84_w.jpg" alt="att samsung garment example 5" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851021556_360427914e_w.jpg" alt="att samsung garmemt example 6" />
-        <img className="object-cover h-full w-auto" height={400} width={100} src="https://live.staticflickr.com/65535/53851575511_531d94abac_w.jpg" alt="att samsung garmemt example 7" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851017261_25305bde46_w.jpg" alt="att samsung garmemt example 8" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851865683_fedbc00bfc_w.jpg" alt="att samsung garmemt example 8.5" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851453880_923c1cbe84_w.jpg" alt="att samsung garment example 5" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851021556_360427914e_w.jpg" alt="att samsung garmemt example 6" />
+        <img className="object-cover h-full w-auto p-2" height={400} width={100} src="https://live.staticflickr.com/65535/53851575511_531d94abac_w.jpg" alt="att samsung garmemt example 7" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851017261_25305bde46_w.jpg" alt="att samsung garmemt example 8" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851865683_fedbc00bfc_w.jpg" alt="att samsung garmemt example 8.5" />
       </div>
 
       <h2 className="p-4 m-2 bg-white/80 rounded text-center dark:text-black font-bold text-overflow-ellipsis overflow-hidden line-clamp-2">
@@ -48,11 +48,11 @@ export default function Samsung() {
       </h2>
 
       <div className="flex overflow-x-auto scroll-smooth m-auto mt-10">
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851384729_8fca511cc9_w.jpg" alt="att samsung garmemt example 9" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53850686547_ba80cbebd4_w.jpg" alt="att samsung garmemt example 10" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851941204_293bd99c20_w.jpg" alt="att samsung garmemt example 11" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851865518_c8b0300b98_w.jpg" alt="att samsung garmemt example 12" />
-        <img className="object-cover h-full w-auto" height={200} width={100} src="https://live.staticflickr.com/65535/53851021961_71ab5c43dd_w.jpg" alt="att samsung garmemt example 13" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851384729_8fca511cc9_w.jpg" alt="att samsung garmemt example 9" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53850686547_ba80cbebd4_w.jpg" alt="att samsung garmemt example 10" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851941204_293bd99c20_w.jpg" alt="att samsung garmemt example 11" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851865518_c8b0300b98_w.jpg" alt="att samsung garmemt example 12" />
+        <img className="object-cover h-full w-auto p-2" height={200} width={100} src="https://live.staticflickr.com/65535/53851021961_71ab5c43dd_w.jpg" alt="att samsung garmemt example 13" />
       </div>
 
       <div className={`h-80 rounded-md relative bg-cover bg-center m-auto w-full md:h-96
