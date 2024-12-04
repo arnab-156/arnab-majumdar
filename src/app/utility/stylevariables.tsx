dacd272a7800588352a9b7dbf3d1dfa12557fa1b
arnab-156 <vendmyt@gmail.com>
2024-12-04 14:26:32 -0500

    images aligned (#54)

--- src/app/utility/stylevariables.tsx
+++ src/app/utility/stylevariables.tsx
@@ -1,4 +1,6 @@
 export const cardWrapperStyle = "m-4 hover:transition hover:duration-300 hover:ease-in-out hover:scale-105";
+export const $cardWrapperStyle = "hover:transition hover:duration-300 hover:ease-in-out hover:scale-105";
+
 export const lotus_pink = '#9d174d';
 export const yellowBackgroundTheme = 'bg-gradient-to-b from-yellow-500 bg-yellow-200/75 hover:bg-yellow-700/75 hover:shadow-lg';
 export const pinkBackgroundTheme = 'bg-gradient-to-b from-pink-500 bg-pink-200/75 hover:bg-pink-700/75 hover:shadow-lg';
