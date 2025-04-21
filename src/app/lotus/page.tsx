231fc6dd667aeaba8ed78658efea8622c1f2819d
arnab-156 <vendmyt@gmail.com>
2025-04-21 18:08:55 -0400

    shorten the id

--- src/app/lotus/page.tsx
+++ src/app/lotus/page.tsx
@@ -139,7 +139,7 @@ export default function Lotus() {
           </Card>
         </div>
 
-        <div className={` ${cardWrapperStyle} m-auto`} id="commanders-palace">
+        <div className={` ${cardWrapperStyle} m-auto`} id="commanders">
           <iframe
             width="560"
             height="315"
