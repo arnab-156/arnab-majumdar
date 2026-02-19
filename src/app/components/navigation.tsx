145d3d5ad74489c4ae2c5742d6100c2b21e42c4c
arnab-156 <vendmyt@gmail.com>
2026-02-19 09:32:37 -0500

    Adjust navigation menu icon/comment behavior

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -12,19 +12,19 @@ export const Navigation = () => {
     return (<nav className="flex flex-col items-center justify-between font-nyu" id="navigation">
         <div className="z-10 w-full items-center justify-between flex justify-content font-mono text-sm lg:text-transparent hover:lg:text-black dark:hover:lg:text-white lg:shadow-2xl lg:shadow-purple-800/40 hover:lg:shadow-none">
             {/* Header on desktop and footer on mobile */}
-            <section className={`group rounded-[36px] fixed flex w-3/4 ml-[13%] sm:ml-0 py-3 justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-300 backdrop-blur-md 
+            <section className={`group rounded-[36px] fixed flex w-3/4 ml-[13%] sm:ml-0 py-3 justify-center border-b border-purple-300 bg-gradient-to-b from-zinc-300 backdrop-blur-md 
              dark:border-neutral-800 dark:bg-purple-800/30 dark:from-inherit 
              sm:w-full lg:w-full lg:static lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 
              hover:w-[96%] hover:sm:w-full hover:ml-2 hover:sm:ml-0 lg:h-24 hover:bg-purple-800 dark:hover:bg-purple-900 hover:opacity-99 
              left-0 bottom-3 lg:top-0 
              transition ease-in-out delay-300 group-hover:duration-500
              `}>
-                <p className="group-hover:hidden lg:hidden">
+                {/* <p className="group-hover:hidden lg:hidden">
                     <button type="button" className="focus:outline-none " aria-label="open menu to navigate">
                         <MenuIcon height="1.5rem" width="1.5rem" color="purple" />
                     </button>
-                </p>
-                <div className="hidden grid-cols-3 group-hover:grid sm:grid-cols-3 sm:gap-2 lg:grid">
+                </p> */}
+                <div className="grid-cols-3 group-hover:grid sm:grid-cols-3 sm:gap-2 lg:grid">
                     <button
                         type="button"
                         onClick={() => router.push("/")}
